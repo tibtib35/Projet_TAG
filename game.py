8dbef73c7a1bb9b8b0e82744bae653b46ab9cd1a
thibault <thibault.huet.bzh@gmail.com>
2026-02-02 17:47:27 +0100

    base loop

--- game.py
+++ game.py
@@ -1,4 +1,20 @@
 import pygame
 
+def game_loop(surface):
+    quitting = False
+    while not quitting:
+        #gestion des événements
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quitting = True
+        #Affichage de la nouvelle image
+
+        pygame.time.delay(20)
+        pygame.display.update()
+
 if __name__ == "__main__":
- pass
+    pygame.init()
+    window = pygame.display.set_mode(size=(800, 500))
+    pygame.display.set_caption("Premier jeu")
+    game_loop(window)
+    pygame.quit()  
